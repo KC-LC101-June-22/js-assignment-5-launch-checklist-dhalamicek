f42382bf63c363866e427efa6fabfb8778a842a0
dhalamicek <110735781+dhalamicek@users.noreply.github.com>
2022-08-30 21:10:26 -0500

    Added code to scriptHelper to validate and create alerts, form submit, updates. Added code to script to include functions.

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Write your JavaScript code here!
-
+//here is where event listener is and bring in functions
+const scriptHelper = require('./scriptHelper.js');
 window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+    
+    let pilotNameInput = document.querySelector("input[name=pilotName]");
+    let copilotNameInput = document.querySelector("input[name=copilotName]");
+    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+    let cargoMassInput = document.querySelector("input[name=cargoMass]");
+    let pilotStatus = form.getElementById("pilotStatus");
+    let copilotStatus = form.getElementByName("copilotStatus");
+    let fuelLevel = form.getElementByName("fuelLevel");
+    let cargoLevel = form.getElementByName("cargoMass");
+
+    scriptHelper()
+
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
@@ -13,4 +28,5 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
+});
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,6 +1,7 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
    /*
@@ -17,12 +18,77 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
-}
+    if (testInput === "") {
+    return "Empty";
+    };
+
+    if (isNan(testInput) === true) {
+    return "Not a Number";
+
+    } else {
+    return "Is a Number";
+    };
+      };
+
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+   //let pilotStatus = form.getElementById("pilotStatus");
+   //let copilotStatus = form.getElementByName("copilotStatus");
+   //let fuelLevel = form.getElementByName("fuelLevel");
+   //let cargoLevel = form.getElementByName("cargoMass");
    
-}
+   if (validateInput() === "Empty") {
+    alert("All fields are required");
+   };
+
+   if (validateInput(pilot) === "Not a Number" || validateInput(copilot) === "Not a Number") {
+    alert("Not a Number");
+   } else {
+    let pilotName = document.getElementById("pilotStatus")
+    pilotName.innerHTML = "<li>`Pilot ${pilot} Ready`</li>"
+    let copilotName = document.getElementById("copilotStatus")
+    copilotName.innerHTML = "<li>`Copilot ${copilot} Ready`</li>"
+   };
+
+   if(validateInput(fuelLevel) === "Is a Number" || validateInput(cargoLevel) === "Is a Number") {
+    alert("Is a Number");
+   };
+
+   if (fuelLevel < 10000) {
+    let launchStop = document.getElementById("launchStatus")
+    launchStop.innerHTML = "<h2 style=color: red;>Shuttle not ready for launch</h2>"
+    let fuelLow = document.getElementById("fuelStatus");
+    fuelLow.innerHTML ="<li>Fuel level too low for launch</li>"
+   };
+
+   if (fuelLevel > 10000) {
+    let fuelGood = document.getElementById("fuelStatus");
+    fuelGood.innerHTML ="<li>Fuel level ready for launch</li>"
+   };
+
+   if (cargoLevel > 10000) {
+    let launchStop = document.getElementById("launchStatus")
+    launchStop.innerHTML = "<h2 style=color: red;>Shuttle not ready for launch</h2>"
+    let cargoHigh = document.getElementById("cargoStatus")
+    cargoHigh.innerHTML ="<li>Cargo mass too high for launch</li>"
+   };
+
+   if (cargoLevel < 10000) {
+    let cargoLow = document.getElementById("cargoStatus")
+    cargoLow.innerHTML ="<li>Cargo mass low enough for launch</li>"
+    };
+
+    if (fuelLevel > 10000 && cargoLevel < 10000) {
+    let launchGo = document.getElementById("launchStatus")
+    launchGo.innerHTML = "<h2 style=color: green;>Shuttle is ready for launch</h2>"
+   };
+};
+
+
+
+
+
+
 
 async function myFetch() {
     let planetsReturned;
